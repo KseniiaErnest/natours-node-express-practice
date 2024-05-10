27b9170c627125191b376ae29375505f943dd900
Kseniia Ernest <hramtcovakseniia@gmail.com>
2024-05-10 11:29:09 -0400

    Unhandled Rejections + Uncaught Exceptions

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
+process.on('uncaughtException', err => {
+  console.log('UNHANDLED EXCEPTION! Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+})
+
 // console.log(process.env);
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
@@ -17,6 +23,15 @@ mongoose.connect(DB).then((con) => {
 
 // Start the server
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
+
+process.on('unhandledRejection', err => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION! Shutting down...');
+  server.close(() => {
+    process.exit(1);
+  })
+});
+
